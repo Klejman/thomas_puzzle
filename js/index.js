4880565d81d424199babd237e01b1f3f467b75f8
Katie Lejman <misskatiemary@gmail.com>
2018-02-14 20:19:49 -0500

    puzzle

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$('.tiles').sortable();
 
-	var shuffle = function(){
-		var tiles = $('.tiles li');
+	const shuffle = function(){
+		let tiles = $('.tiles li');
 
 		for(var i = 0, length = tiles.length; i < length; i++){
 			var randIndex = Math.floor(Math.random() * (length - i) + i);
